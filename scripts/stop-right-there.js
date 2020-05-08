0c8c58a844f0f9658c6f67a6aefb4b2c0de2e0af
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-08 16:41:44 +0900

    Update stop-right-there.js

--- scripts/stop-right-there.js
+++ scripts/stop-right-there.js
@@ -1,7 +1,7 @@
 const stoprightthere = extendContent(Block, "stop-right-there", {
   placed(tile) {
     this.super$placed(tile);
-    Vars.unitGroup.all().copy().each(cons(ent => {
+    Vars.unitGroup.all().each(cons(ent => {
       ent.kill();
     }));
   }
